8086eb1b5751f8294c3802dcd9778b06f3142e1a
m2x07 <meet2x07@gmail.com>
2023-10-30 22:40:42 +0530

    minor changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.sass";
 export default function Footer() {
   return (
     <footer>
-      by <a href="http://https://twitter.com/_m2x07" target="_blank" rel="noopener noreferrer">m2x07</a>{" | "}
+      by <a href="https://twitter.com/_m2x07" target="_blank" rel="noopener noreferrer">m2x07</a>{" | "}
       view on{" "}
       <a href="http://github.com/m2x07/tic-tac-toe" target="_blank" rel="noopener noreferrer">
         github
