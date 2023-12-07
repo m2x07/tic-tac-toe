021414b7e43380ea3d479b27acdf7585905a02f6
m2x07 <meet2x07@gmail.com>
2023-12-07 21:11:29 +0530

    feat: add icons support, for both, board and game status

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,6 @@
 import propTypes from "prop-types";
 // import './Square.sass';
+import { IconCircle, IconX } from "@tabler/icons-react";
 
 function Square({ value, onSquareClick }) {
   Square.propTypes = {
@@ -7,13 +8,20 @@ function Square({ value, onSquareClick }) {
     onSquareClick: propTypes.func,
     fontColor: propTypes.string,
   };
-
+  let icon = "";
+  if (value == null) {
+    icon = null;
+  } else if (value == "X") {
+    icon = <IconX color="#EF3E36" size="36" />;
+  } else {
+    icon = <IconCircle color="#F9C639" size="31"/>;
+  }
   return (
     <button
-      className="neumorphic-shadow-inset m-1 h-20 w-20 items-center justify-center rounded-lg font-display text-[length:38px] tracking-tighter"
+      className="neumorphic-shadow-inset m-1 flex h-20  w-20 items-center justify-center rounded-lg font-display text-[length:38px] tracking-tighter"
       onClick={onSquareClick}
     >
-      {value}
+      {icon}
     </button>
   );
 }
